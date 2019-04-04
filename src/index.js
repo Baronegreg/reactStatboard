67e973a7447a9521fd252fdbdaa4b53e5884147d
Baronegreg <barone_greg@yahoo.com>
2019-04-04 17:40:54 -0600

    connecting to POC

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CoachReport from './CoachReport/CoachReport'
+import {socket} from './Socket'
 
-render((
+if (document.getElementById('root')) {
+  render((
     <BrowserRouter>
       <App />
     </BrowserRouter>
   ), document.getElementById('root'));
+}
+
+  window.XDomain = {
+    // http://localhost:3000/XDomain/CoachReport/
+    CoachReport : CoachReport,
+    socket : socket,
+    React,
+    // Export render
+    render
+
+  }
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
